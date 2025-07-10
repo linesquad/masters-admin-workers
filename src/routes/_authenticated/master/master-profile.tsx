92a2187580ff0c4da672844708c5b23671b7d2b9
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-07-10 19:30:13 +0400

    implement fix

--- src/routes/_authenticated/master/master-profile.tsx
+++ src/routes/_authenticated/master/master-profile.tsx
@@ -1,7 +1,15 @@
 import { MasterProfileView } from "@/modules/master/profile/ui/views/master-profile-view";
+import { redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute({
   component: RouteComponent,
+  loader: async ({ context }) => {
+    const user = await context.getUser();
+    if (user.role !== "master") {
+      throw redirect({ to: "/" });
+    }
+    return { user };
+  },
 });
 
 function RouteComponent() {
